bb3027a6a60c90b1ed30a199c8584cf4bd73be4d
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-09 05:54:23 +1100

    lift state, useEffect & onStartStop method from Timer to App level

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Break from "./components/Break";
 import Session from "./components/Session";
 import Timer from "./components/Timer";
 
 function App() {
+	const [intervalId, setIntervalId] = useState(null);
+	const [isBreakTime, setIsBreakTime] = useState(false);
 	const [breakLength, setBreakLength] = useState(300);
 	const [sessionLength, setSessionLength] = useState(60 * 25);
+	const [timeLeft, setTimeLeft] = useState(sessionLength);
+	const isActive = intervalId !== null;
+
+	useEffect(() => setTimeLeft(sessionLength), [sessionLength]);
 
 	const decrementBreakLength = () => {
 		const newBreakLength = breakLength - 60;
 		newBreakLength < 0 ? setBreakLength(0) : setBreakLength(newBreakLength);
 	};
+
 	const decrementSessionLength = () => {
 		const newSessionLength = sessionLength - 60;
 		newSessionLength < 0
 			? setSessionLength(0)
 			: setSessionLength(newSessionLength);
 	};
+
 	const incrementBreakLength = () => setBreakLength(breakLength + 60);
 	const incrementSessionLength = () => setSessionLength(sessionLength + 60);
+
+	const onStartStop = () => {
+		if (isActive) {
+			clearInterval(intervalId);
+			setIntervalId(null);
+		} else {
+			const newIntervalId = setInterval(() => {
+				setTimeLeft(prevTimeLeft => {
+					const newTimeLeft = prevTimeLeft - 1;
+					if (newTimeLeft >= 0) {
+						return newTimeLeft;
+					}
+					if (!isBreakTime) {
+						setTimeLeft(breakLength);
+						setIsBreakTime(true); //! this switch doesn't seem to be working
+					}
+					if (isBreakTime) {
+						setTimeLeft(sessionLength);
+						setIsBreakTime(false); //! this switch doesn't seem to be working
+					}
+				});
+			}, 100); // TODO: change to '1000'
+			setIntervalId(newIntervalId);
+		}
+	};
+
+	const onReset = () => {
+		// clear the timer interval
+		// set intervalId to null
+		// set isBreakTime to false
+		// reset sessionLength to 25min
+		// reset breakLength to 5min
+		// reset the timer to 25min
+	};
 
 	return (
 		<div className="App">
@@ -33,7 +75,15 @@ function App() {
 				decrementSessionLength={decrementSessionLength}
 				incrementSessionLength={incrementSessionLength}
 			/>
-			<Timer sessionLength={sessionLength} breakLength={breakLength} />
+			<Timer
+				sessionLength={sessionLength}
+				breakLength={breakLength}
+				isBreakTime={isBreakTime}
+				isActive={isActive}
+				timeLeft={timeLeft}
+				onStartStop={onStartStop}
+				onReset={onReset}
+			/>
 		</div>
 	);
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,53 +1,34 @@
-import { useState, useEffect } from "react";
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
 import { FaPlay, FaPause, FaRedo } from "react-icons/fa";
 
 momentDurationFormatSetup(moment);
 
-const Timer = ({ sessionLength, breakLength }) => {
-	const [intervalId, setIntervalId] = useState(null);
-	const [timeLeft, setTimeLeft] = useState(sessionLength);
-	const [isBreakTime, setIsBreakTime] = useState(false);
+const Timer = ({
+	sessionLength,
+	breakLength,
+	isBreakTime,
+	isActive,
+	timeLeft,
+	onStartStop,
+	onReset,
+}) => {
 	const formattedTimeLeft = moment
 		.duration(timeLeft, "s")
 		.format("mm:ss", { trim: false });
-	const isActive = intervalId !== null;
-
-	useEffect(() => setTimeLeft(sessionLength), [sessionLength]);
-
-	const onStartStop = () => {
-		if (isActive) {
-			clearInterval(intervalId);
-			setIntervalId(null);
-		} else {
-			const newIntervalId = setInterval(() => {
-				setTimeLeft(prevTimeLeft => {
-					const newTimeLeft = prevTimeLeft - 1;
-					if (newTimeLeft >= 0) {
-						return newTimeLeft;
-					}
-					if (!isBreakTime) {
-						setTimeLeft(breakLength);
-						setIsBreakTime(true); //! this switch doesn't seem to be working
-					}
-					if (isBreakTime) {
-						setTimeLeft(sessionLength);
-						setIsBreakTime(false); //! this switch doesn't seem to be working
-					}
-				});
-			}, 100); // TODO: change to '1000'
-			setIntervalId(newIntervalId);
-		}
-	};
 
 	return (
-		<div>
+		<div id="timer">
 			<h2 id="timer-label">{isBreakTime ? "Break" : "Session"}</h2>
 			<p id="time-left">{formattedTimeLeft}</p>
-			<button onClick={onStartStop}>
-				{isActive ? <FaPause /> : <FaPlay />}
-			</button>
+			<div id="timer-controls">
+				<button id="start_stop" onClick={onStartStop}>
+					{isActive ? <FaPause /> : <FaPlay />}
+				</button>
+				<button id="reset" onClick={onReset} style={{ marginLeft: "30px" }}>
+					<FaRedo />
+				</button>
+			</div>
 		</div>
 	);
 };
